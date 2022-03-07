105a750b991f6f2239b553c7675ae15bbfecfa2d
IgoBeza <igobezerra@gmail.com>
2022-03-06 21:34:45 -0300

    Initial commit

---
+++ src/test/java/screenShot.java
@@ -0,0 +1,64 @@
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.By;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.io.FileHandler;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import java.io.File;
+import java.io.IOException;
+
+
+public class screenShot {
+WebDriver driver;
+
+@BeforeMethod
+    public void setUp( ) {
+
+    WebDriverManager.firefoxdriver().setup();
+    driver = new FirefoxDriver();
+    driver.manage().window();
+    driver.get("https://applitools.com/");
+
+}
+
+@Test
+    public void screenshotWebElement() throws IOException {
+
+    WebElement nextGenerationPlatform =
+            driver.findElement(By.cssSelector("#post-8 h1"));
+    File source = nextGenerationPlatform.getScreenshotAs(OutputType.FILE);
+    File destination = new File( "Next Generation Platform.png");
+    FileHandler.copy(source, destination);
+
+}
+
+
+
+@Test
+   public void takeWebElementPageSectionScreenshot() throws IOException {
+
+    WebElement appliesPageSection = driver.findElement(By.cssSelector("#post-8>header"));
+    File source = appliesPageSection.getScreenshotAs(OutputType.FILE);
+    FileUtils.copyFile(source, new File("Applitools Page Section.png"));
+
+
+}
+@Test
+    public void takeFullPageScreenshot() throws IOException {
+
+    //getFullPageScreenshotAs so o browser "firefox"
+
+     File source = ((FirefoxDriver)driver).getFullPageScreenshotAs(OutputType.FILE);
+    FileHandler.copy(source, new File("Applitool Full Page Screenshot.png"));
+
+}
+
+}
